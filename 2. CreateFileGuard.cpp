5d671b6ffb4b44b2f13249a0232bb90e1b9bd593
x64x86 <dima.yurko@gmail.com>
2018-10-04 13:10:32 +0300

    Update 2. CreateFileGuard.cpp

--- 2. CreateFileGuard.cpp
+++ 2. CreateFileGuard.cpp
@@ -1,15 +1,57 @@
-#include "stdafx.h"
+#include "pch.h"
 #include <windows.h>
 #include <iostream>
 #include <string>
 
-class CreateFileGuard
+class GFile
 {
 public:
-	CreateFileGuard(HANDLE handle) : hFile(handle) {}
-	~CreateFileGuard()
+	GFile(HANDLE handle) 
 	{
-		CloseHandle(hFile);
+		if (isValidHandle(handle))
+		{
+			hFile = handle;
+			std::cout << "Guard init with handle: " << hFile << std::endl;
+		}
+		else
+		{
+			hFile = NULL;
+		}
+	}
+	GFile& operator= (HANDLE handle)
+	{
+		if (hFile)
+		{
+			std::cout << "Handle closed: " << hFile << std::endl;
+			CloseHandle(hFile);
+		}
+		if (isValidHandle(handle))
+		{
+			hFile = handle;
+			std::cout << "New handle acquired: " << hFile << std::endl;
+		}
+		else
+		{
+			hFile = NULL;
+		}
+		return *this;
+	}
+	~GFile()
+	{
+		if (hFile)
+		{
+			std::cout << "Handle closed: " << hFile << std::endl;
+			CloseHandle(hFile);
+		}
+	}
+	bool isValidHandle(HANDLE handle)
+	{
+		if (handle == INVALID_HANDLE_VALUE)
+		{
+			std::cout << "Invalid file handle" << std::endl;
+			return FALSE;
+		}
+		return TRUE;
 	}
 private:
 	HANDLE hFile;
@@ -17,23 +59,28 @@ private:
 
 int main()
 {
-	LPCWSTR fname = L"test_file";
-	HANDLE hFile;
-	hFile = CreateFile(fname,
-		GENERIC_READ|GENERIC_WRITE,
-		0, 
+	LPCWSTR file1 = L"test_file_1";
+	LPCWSTR file2 = L"test_file_2";
+	HANDLE hFile1;
+	hFile1 = CreateFile(file1,
+		GENERIC_WRITE,
+		0,
 		NULL,
-		CREATE_NEW|OPEN_EXISTING,
+		CREATE_ALWAYS,
 		FILE_ATTRIBUTE_NORMAL,
 		NULL);
-	
-	if (hFile == INVALID_HANDLE_VALUE)
-	{
-		std::cout << "Failed to open file" << std::endl;
-		return 1;
-	}
 
-	CreateFileGuard gFile(hFile);
+	GFile gFile(hFile1);
+
+	HANDLE hFile2;
+	hFile2 = CreateFile(file2,
+		GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_NEW,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+	gFile = hFile2;
 
 	std::cin.ignore();
 	return 0;
